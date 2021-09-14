590a87f61da0a041a4bf6c423f121e88006ee211
Danny Flores <floresfredj@gmail.com>
2021-09-14 18:52:14 -0500

    final

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,9 +17,37 @@ namespace mvcpc.Controllers
         {
             _logger = logger;
         }
+        public IActionResult Formulario() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Formulario(Formulario formulario){
+            // Guardarlo en una BD
+            // Enviar un correo
+            // No hacer nada
+            Console.WriteLine(formulario.NombreCompleto);
+            Console.WriteLine(formulario.TipMascota);
+            Console.WriteLine(formulario.Descripcion);
+            Console.WriteLine(formulario.Telefono);
+            Console.WriteLine(formulario.Distrito);
+            Console.WriteLine(formulario.Direccion);
+            return RedirectToAction("Confirmacion");
+            //return View("ContactoConfirmacion");
+        }
+
+        public IActionResult Confirmacion(string param1,string mensaje, int n) {
+            ViewData["param1"] = n;
+            ViewData["mensaje"] = mensaje;
+            return View();
+        }
 
         public IActionResult Index()
         {
+            //ViewData["param1"] = n;
+            //ViewData["mensaje"] = mensaje;
+            // return View();
+            //return RedirectToAction("Formulario");
             return View();
         }
 
---
+++ Models/Formulario.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace mvcpc.Models
+{
+    public class Formulario
+    {
+        public string NombreCompleto { get; set; }
+        public string TipMascota{ get; set; }
+        public string Descripcion{ get; set; }
+        public string Telefono { get; set; }
+        public string Distrito{ get; set; }
+        public string Direccion { get; set; }
+    }
+}
